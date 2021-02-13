3444ac99e83b276d2d15c121c982e953a11dc255
zotov_dv <zotov_dv@proitr.ru>
2021-02-13 21:36:08 +0300

    Added interface project

---
+++ PressorUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Pressor.Logic;
+using System.Windows;
+
+namespace PressorUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var pp = new PressorParameters();
+            Resources.Add("pp", pp);
+
+        }
+    }
+}
